a16e7044e25e35b8e8b8c620b79e4a0d1506bada
markus-ro <132851390+markus-ro@users.noreply.github.com>
2026-01-11 01:49:51 +0100

    day seven star two

--- 2025/day7/day_seven.py
+++ 2025/day7/day_seven.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 def solution_star_one(manifold):
     _m = [list(x) for x in manifold]
-    print(_m)
     _splits = 0
 
     for row in range(1, len(_m)):
@@ -20,17 +19,26 @@ def solution_star_one(manifold):
     return _splits
 
 def solution_star_two(manifold):
-    _m = [list(x) for x in manifold][:-1]
+    _m = [list(x) for x in manifold]
+    _paths = [[0 for _ in range(len(_m[0]))] for _ in range(len(_m))]
 
-    for i in range(len(_m[0])): 
-        _m[-1][i] = 2 if _m[-1][i] == "^" else 0
-
-    return 0
+    for row in range(len(_m)):
+        for col in range(len(_m[0])):
+            if _m[row][col] == "S":
+                _paths[row][col] = 1
+            elif _m[row][col] == "^":
+                _paths[row + 1][col - 1] += _paths[row - 1][col]
+                _paths[row + 1][col + 1] += _paths[row - 1][col]
+            else:
+                _paths[row][col] += _paths[row - 1][col] if row > 0 else 0
+                
+    return sum(_paths[-1])
 
 if __name__ == "__main__":
     tachyon_manifold = [x.strip() for x in Path(argv[1]).open().readlines()]
 
-    print("Star 1:", solution_star_one(tachyon_manifold))
+    _splits = solution_star_one(tachyon_manifold)
+    print("Star 1:", _splits)
 
     print("Star 2:", solution_star_two(tachyon_manifold))
 
