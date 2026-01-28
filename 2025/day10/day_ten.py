e695601e383d627bdf9ac7b175511e8a0d790c60
markus-ro <132851390+markus-ro@users.noreply.github.com>
2026-01-28 20:10:39 +0100

    day ten star two

--- 2025/day10/day_ten.py
+++ 2025/day10/day_ten.py
@@ -1,3 +1,5 @@
+import pulp as pl
+
 from sys import argv
 from pathlib import Path
 
@@ -18,7 +20,22 @@ def solution_star_one(machines):
     return fewest_attempts
 
 def solution_star_two(machines):
-    pass
+    minimum_presses = 0
+    
+    for machine in machines:
+        problem = pl.LpProblem("MachineJolt", pl.LpMinimize)
+        vars = [pl.LpVariable(f"x{i}", cat="Integer", lowBound=0)
+                for i in range(len(machine.vectors))]
+        problem += pl.lpSum(vars)
+
+        for i, jolt in enumerate(machine.joltage):
+            problem += pl.lpSum(vars[j] * vec[i] for j, vec in enumerate(machine.vectors)) == jolt
+
+        problem.solve(solver=pl.PULP_CBC_CMD(msg=False))
+        minimum_presses += int(problem.objective.value())
+
+    return minimum_presses
+
 
 if __name__ == "__main__":
     machines = parse_machines(Path(argv[1]))
--- 2025/day10/utils.py
+++ 2025/day10/utils.py
@@ -14,14 +14,23 @@ def button_to_int(button, display_size):
     button_as_tuple = eval(button)
     for n in button_as_tuple:
         buffer[n] = "#"
-
     return display_to_int("[" + "".join(buffer) + "]")[0]
+
+def button_to_vector(button, dim):
+    button = button[:-1] + ",)"
+    vec = [0] * dim
+    button_as_tuple = eval(button)
+    for n in button_as_tuple:
+        vec[n] = 1
+
+    return vec
 
 class Machine:
     def __init__(self, target, buttons, joltage):
         self.target, self.size = display_to_int(target)
+        self.joltage = eval(joltage.replace("{", "[").replace("}", "]"))
         self.buttons = [button_to_int(x, self.size) for x in buttons]
-        self.joltage = eval(joltage)
+        self.vectors = [button_to_vector(x, len(self.joltage)) for x in buttons]
 
 def parse_machines(input_file: Path):
     machines = []
