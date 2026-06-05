1270e2c618f60160a865c67c23f2063f7a38840b
markus-ro <132851390+markus-ro@users.noreply.github.com>
2026-06-06 00:49:14 +0200

    day six star onw

---
+++ 2024/day_six.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from sys import argv
+from collections import defaultdict
+
+def parse_input(file: Path) -> tuple[list[list[str]], tuple[int, int]]:
+    map = []
+    position = None
+    with file.open() as f:
+        for i, line in enumerate(f.readlines()):
+            map.append(list(line.strip()))
+            if "^" in line: position = (i, line.index("^"))
+
+    return map, position
+    
+def solution_star_one(map: list[list[str]], start_position: tuple[int, int]) -> dict[tuple[int, int], tuple[int, int]]:
+    def turn_right(direction: tuple[int, int]) -> tuple[int, int]:
+        return (direction[1], -direction[0])
+
+    direction = (-1, 0)
+    positions = {start_position: direction}
+
+    while True:
+        next_position = (start_position[0] + direction[0], start_position[1] + direction[1])
+        if next_position[0] < 0 or next_position[0] >= len(map) or next_position[1] < 0 or next_position[1] >= len(map[0]):
+            break
+        elif map[next_position[0]][next_position[1]] == "#":
+            direction = turn_right(direction)
+        elif map[next_position[0]][next_position[1]] in [".", "^"]:
+            start_position = next_position
+            positions[start_position] = direction
+        else:
+            break
+        
+    return positions
+
+def solution_star_two(map: list[list[str]], start_position: tuple[int, int]) -> int:
+    def turn_right(direction: tuple[int, int]) -> tuple[int, int]:
+        return (direction[1], -direction[0])
+
+    positions = solution_star_one(map, start_position)
+    circles = 0
+    
+    return circles
+
+if __name__ == "__main__":
+    map, position = parse_input(Path(argv[1]))
+    print("Solution Star One:", len(solution_star_one(map, position)))
+    print("Solution Star Two:", solution_star_two(map, position))
